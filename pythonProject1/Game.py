1dc36c7ce77449c1ed1719bd0474188724f1577c
sharonij <sharonagwah@gmail.com>
2022-06-12 22:09:53 +0100

    initial commit

---
+++ pythonProject1/Game.py
@@ -0,0 +1,55 @@
+import random
+
+while True:
+    choices = ["rock","paper","scissors"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock, paper, or scissors?: ").lower()
+
+    if player == computer:
+        print("computer: ",computer)
+        print("player: ", player)
+        print("Tie!")
+
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+            break
+
+    elif player == "scissors":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+            Break
+
+    elif player == "paper":
+        if computer == "scissors":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+            Break
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+
+print("Thanks for playing!")
